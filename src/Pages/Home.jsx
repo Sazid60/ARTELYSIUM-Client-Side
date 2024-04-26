d9d3140a0d27ccb3692fa764873957a70b1a590c
Sazid60 <shahnawazsazid60@gmail.com>
2024-04-26 13:15:50 +0600

    Banner Section & React-Simple TypeWriter Added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Components/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <h1>Thi is Home</h1>
+            <Banner></Banner>
         </div>
     );
 };
