cd269632c113b3f4a6d9af81140aa8243ff78902
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-10-22 07:11:31 +0600

    React Version Upgraded

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
 
@@ -13,10 +13,11 @@ import AuthProvider from './AuthProvider/AuthProvider';
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
+
